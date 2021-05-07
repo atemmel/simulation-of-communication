d54e082ec4202a8a0cf34fd59f59df9310db200b
atemmel <temmeladam@gmail.com>
2021-05-07 19:08:25 +0200

    Added a few results

--- project/project.cc
+++ project/project.cc
@@ -1,6 +1,7 @@
 #include "ns3/core-module.h"
 
 #include <algorithm>
+#include <cmath>
 #include <fstream>
 #include <vector>
 
@@ -25,6 +26,10 @@ double normalize(T x, T min, T max) {
 	return static_cast<double>(x - min) / static_cast<double>(max - min);
 }
 
+double expDist(double value, double lambda) {
+	return -log(1 - value) / lambda;
+}
+
 int main(int argc, char** argv) {
 	Lcg lcg(1);
 
@@ -32,19 +37,26 @@ int main(int argc, char** argv) {
 	lcg.m = std::numeric_limits<Lcg::seed_t>::max();
 	lcg.c = 0u;
 
-	Ptr<UniformRandomVariable> urv = CreateObject<UniformRandomVariable> ();
+	// our class: time ./waf --run scratch/project  2.54s user 0.20s system 107% cpu 2.547 total
+	// their class: time ./waf --run scratch/project  2.59s user 0.17s system 107% cpu 2.556 total
+
+	//Ptr<UniformRandomVariable> urv = CreateObject<UniformRandomVariable> ();
+	Ptr<ExponentialRandomVariable> erv = CreateObject<ExponentialRandomVariable> ();
 
 	constexpr size_t n = 1000;
+	constexpr double lambda = 300.;
 
 	std::vector<double> ourResults(n);
-	std::vector<double> theirResults(n);
 
 	std::generate(ourResults.begin(), ourResults.end(), [&]() {
-		return normalize(lcg(), 0u, lcg.m);
+		return expDist(normalize(lcg(), 0u, lcg.m), lambda);
+		//return expDist(lcg(), lambda);
 	});
 
+	std::vector<double> theirResults(n);
+
 	std::generate(theirResults.begin(), theirResults.end(), [&]() {
-		return urv->GetValue(0., 1.);
+		return erv->GetValue(1.0/lambda, 1.);
 	});
 
 	{
@@ -61,6 +73,7 @@ int main(int argc, char** argv) {
 		}
 	}
 
+	/*
 	std::cout << "Our results: {";
 	for(auto f : ourResults) {
 		std::cout << f << ", ";
@@ -72,4 +85,5 @@ int main(int argc, char** argv) {
 		std::cout << f << ", ";
 	}
 	std::cout << "\b\b}\n";
+	*/
 }
