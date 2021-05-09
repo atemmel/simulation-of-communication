b54ee95e8b6656cbcc55688ade45c9ce10b19ded
atemmel <temmeladam@gmail.com>
2021-05-09 20:56:39 +0200

    :^)

--- project/project.cc
+++ project/project.cc
@@ -5,6 +5,7 @@
 #include "ns3/traffic-control-module.h"
 #include "ns3/flow-monitor-module.h"
 #include "ns3/applications-module.h"
+#include "ns3/netanim-module.h"
 
 #include <algorithm>
 #include <cmath>
@@ -94,6 +95,59 @@ void testPrng() {
 	*/
 }
 
+static void receivedMsg (Ptr<Socket> router, Ptr<Socket> client, Ptr<const Packet> p, const Address &srcAddress , const Address &dstAddress)
+{
+	//std::cout << "::::: A packet received at the Server! Time:   " << Simulator::Now ().GetSeconds () << std::endl;
+
+	Ptr<UniformRandomVariable> rand=CreateObject<UniformRandomVariable>();
+
+	if(rand->GetValue(0.0,1.0) <= 0.3) {
+		//std::cout << "::::: Transmitting from Server to Router   "  << std::endl;
+		router->Send (Create<Packet> (p->GetSize ()));
+	}
+	else {	// P(0.7)
+		//std::cout << "::::: Transmitting from GW to Controller   "  << std::endl;
+		client->SendTo(Create<Packet> (p->GetSize ()),0,srcAddress);
+	}
+}
+
+uint32_t globalN = 0; // hehe
+uint32_t globalSum = 0;
+
+void tcPacketsInQueue (QueueDiscContainer qdiscs, Ptr<OutputStreamWrapper> stream)
+{
+	//Get current queue size value and save to file.
+	uint32_t currentSum = 0;
+	for(uint32_t i = 0; i < qdiscs.GetN(); i++) {
+		Ptr<QueueDisc> p = qdiscs.Get (1);
+		uint32_t size = p->GetNPackets(); 
+		currentSum += size;
+		globalSum += size;
+	}
+	globalN++;
+
+	std::cout << currentSum << '\n';
+
+	*stream->GetStream () << Simulator::Now ().GetSeconds () << "\t" << currentSum << std::endl;
+}
+
+static void generateTraffic (Ptr<Socket> socket, Ptr<ExponentialRandomVariable> randomSize,	Ptr<ExponentialRandomVariable> randomTime)
+{
+	uint32_t pktSize = randomSize->GetInteger (); //Get random value for packet size
+	//std::cout << "::::: A packet is generate at Node "<< socket->GetNode ()->GetId () << " with size " << pktSize <<" bytes ! Time:   " << Simulator::Now ().GetSeconds () << std::endl;
+
+	
+	// We make sure that the message is at least 12 bytes. The minimum length of the UDP header. We would get error otherwise.
+	if(pktSize<12){
+		pktSize=12;
+	}
+
+	socket->Send (Create<Packet> (pktSize));
+
+	Time pktInterval = Seconds(randomTime->GetValue ()); //Get random value for next packet generation time 
+	Simulator::Schedule(pktInterval, &generateTraffic, socket, randomSize, randomTime); //Schedule next packet generation
+}
+
 class NetworkTopology {
 public:
 	struct Binding {
@@ -147,7 +201,47 @@ public:
 		return ptp.Install(nodes.Get(binding.first), nodes.Get(binding.second));
 	}
 
+	uint32_t setSource(uint32_t nodeId, uint32_t addressId, uint32_t port) {
+		TypeId tid = TypeId::LookupByName ("ns3::UdpSocketFactory");
+		Ptr<Socket> source = Socket::CreateSocket(nodes.Get(nodeId), tid);
+		auto target = ipv4container.GetAddress(addressId);
+		InetSocketAddress remote = InetSocketAddress(target, port);
+		source->Connect(remote);
+		sockets.push_back(source);
+		return sockets.size() -1;
+	}
 
+	uint32_t setSource(uint32_t nodeId, uint32_t port) {
+
+		TypeId tid = TypeId::LookupByName ("ns3::UdpSocketFactory");
+		Ptr<Socket> source = Socket::CreateSocket(nodes.Get(nodeId), tid);
+		sockets.push_back(source);
+		return sockets.size() -1;
+	}
+
+	void installServer(uint32_t nodeId, uint32_t port) {
+		UdpServerHelper server(port);
+		servers.Add(server.Install(nodes.Get(nodeId)));
+	}
+
+	void startSendingSomePackets(uint32_t socketId, double time, double size) {
+		//Mean inter-transmission time
+		Ptr<ExponentialRandomVariable> randomTime = CreateObject<ExponentialRandomVariable> ();
+		randomTime->SetAttribute ("Mean", DoubleValue (time));
+
+		//Mean packet time q8^)
+		//mean = 100; // 100 Bytes
+		Ptr<ExponentialRandomVariable> randomSize = CreateObject<ExponentialRandomVariable> ();
+
+		randomSize->SetAttribute ("Mean", DoubleValue (size));
+
+		auto source = sockets[socketId];
+
+		Simulator::ScheduleWithContext (source->GetNode ()->GetId (), Seconds (2.0), &generateTraffic, source, randomSize, randomTime);
+	}
+
+    ApplicationContainer servers;
+	std::vector<Ptr<Socket>> sockets;
 	Ipv4AddressHelper ipv4;
 	Ipv4InterfaceContainer ipv4container;
 	QueueDiscContainer qdiscs;
@@ -205,30 +299,81 @@ int main(int argc, char** argv) {
 	 *                 | 2.1
 	 *                 C
 	 */
-
-	/*
-	for(uint32_t i = 0; i < topology.ipv4container.GetN(); i++) {
-		std::cout << i << '\n';
-		auto nint = topology.ipv4container.Get(i).first->GetNInterfaces();
-		for(uint32_t j = 0; j < nint; j++) {
-			std::cout << '\t' << topology.ipv4container.Get(i).first->GetAddress(j, 0) << '\n';
-		}
-	}*/
 	
     uint16_t port_number = 9;  
+	/*
     ApplicationContainer server_apps;
 	UdpServerHelper serverS (port_number);
 	server_apps.Add(serverS.Install(topology.nodes.Get(7)));
 
 	Ptr<UdpServer> S1 = serverS.GetServer();
+	*/
 
-	TypeId tid = TypeId::LookupByName ("ns3::UdpSocketFactory");
+	// Set up sources
 
-	Ptr<Socket> source1 = Socket::CreateSocket(topology.nodes.Get(7),tid);
-	InetSocketAddress remote1 = InetSocketAddress (topology.ipv4container.GetAddress(15), port_number);
-	source1->Connect (remote1);
+	// From server to router
+	auto routerSocketIndex = topology.setSource(7, 15, port_number);
 
+	// From server to clients
+	auto clientSocketIndex = topology.setSource(7, port_number);
+
+	// From client to server
+	auto aSocketIndex = topology.setSource(0, 13, port_number);
+	auto bSocketIndex = topology.setSource(1, 13, port_number);
+	auto cSocketIndex = topology.setSource(2, 13, port_number);
+	auto dSocketIndex = topology.setSource(3, 13, port_number);
+
+	// Set up servers
+	topology.installServer(0, port_number);
+	topology.installServer(1, port_number);
+	topology.installServer(2, port_number);
+	topology.installServer(3, port_number);
+	topology.installServer(7, port_number);
+	topology.installServer(8, port_number);
+
+	auto routerSocket = topology.sockets[routerSocketIndex];
+	auto clientSocket = topology.sockets[clientSocketIndex];
+
+	auto server = topology.servers.Get(4);
+	server->TraceConnectWithoutContext ("RxWithAddresses", MakeBoundCallback (&receivedMsg, routerSocket, clientSocket));
+
+	topology.startSendingSomePackets(aSocketIndex, 0.002, 100.);
+	topology.startSendingSomePackets(bSocketIndex, 0.002, 100.);
+	topology.startSendingSomePackets(cSocketIndex, 0.0005, 100.);
+	topology.startSendingSomePackets(dSocketIndex, 0.001, 100.);
+
+	AnimationInterface anim("out.xml");
+	anim.EnablePacketMetadata(true);
+	anim.SetConstantPosition(topology.nodes.Get(0), -100, -100);
+	anim.SetConstantPosition(topology.nodes.Get(1), -100, -0);
+	anim.SetConstantPosition(topology.nodes.Get(2), -50, +100);
+	anim.SetConstantPosition(topology.nodes.Get(3),   0, +100);
+	anim.SetConstantPosition(topology.nodes.Get(4), -50, -100);
+	anim.SetConstantPosition(topology.nodes.Get(5), -50, -0);
+	anim.SetConstantPosition(topology.nodes.Get(6), 0, -0);
+	anim.SetConstantPosition(topology.nodes.Get(7), 0, -100);
+	anim.SetConstantPosition(topology.nodes.Get(8), 100, -0);
+
+	double simulationTime = 10.0;
+
+	AsciiTraceHelper asciiTraceHelper;
+	Ptr<OutputStreamWrapper> stream = asciiTraceHelper.CreateFileStream ("project_queue.tr");
+
+	for (float t=1.0; t < simulationTime; t+=0.001) {
+		Simulator::Schedule(Seconds(t), &tcPacketsInQueue, topology.qdiscs, stream);
+	}
+
+	Simulator::Stop(Seconds(simulationTime));
+	Simulator::Run();
+
+	Simulator::Destroy();
+
+	/*
 	for(uint32_t i = 0; i < topology.ipv4container.GetN();i++) {
 		std::cout << topology.ipv4container.GetAddress(i) << '\n';
 	}
+	*/
+
+	std::cout << globalSum << ' ' << globalN << '\n';
+	std::cout << static_cast<double>(globalSum) / static_cast<double>(globalN) << '\n';
 }
